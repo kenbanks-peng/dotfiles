f8db512a569a8f9e059d324441ec514cc1af66e6
Ken Banks <kenbanks.peng@gmail.com>
2025-08-20 20:10:34 -0400

    commit

--- wezterm/config/startup.lua
+++ wezterm/config/startup.lua
@@ -1,3 +1,11 @@
-return {
-   default_prog = { '/opt/homebrew/bin/zellij' },
-}
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local config = {}
+
+wezterm.on('gui-startup', function(cmd)
+   local tab, pane, window = mux.spawn_window {
+      args = { 'zsh', '-l', '-c', '/opt/homebrew/bin/zellij' },
+   }
+end)
+
+return config
