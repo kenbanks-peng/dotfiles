bdffd729140d5e7c127ac9a1b680e394246d2924
Ken Banks <kenbanks.peng@gmail.com>
2025-09-05 22:30:01 -0400

    dotfiles backup on 2025-09-05 22:30:01

--- wezterm/config/startup.lua
+++ wezterm/config/startup.lua
@@ -1,12 +1,12 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
 local mux = wezterm.mux
 local config = {}
 
--- args = { 'zsh', '-l', '-c', '/opt/homebrew/bin/zellij' },
-wezterm.on('gui-startup', function()
-   mux.spawn_window {
-      cwd = '/Users/ken/Software/',
-   }
-end)
+-- -- args = { 'zsh', '-l', '-c', '/opt/homebrew/bin/zellij' },
+-- wezterm.on('gui-startup', function()
+--    mux.spawn_window {
+--      cwd = '/Users/ken/Software/',
+--   }
+-- end)
 
 return config
