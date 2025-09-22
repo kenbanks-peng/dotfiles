b0f458d8a3e2c7358ef525cdf5858d5836158c07
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-09-22 15:43:22 -0400

    .

--- yazi/plugins/smart-enter.yazi/main.lua
+++ yazi/plugins/smart-enter.yazi/main.lua
@@ -1,10 +1,7 @@
 return {
   entry = function()
-    -- Use the correct "enter" command which navigates into directories
-    -- but does nothing on files (just like pressing 'l' or right arrow)
+    -- enters directory but does nothing on file
     ya.emit("enter", {})
-
-    -- Use the same quit command as the Q key binding to avoid confirmation
-    ya.emit("quit", { "--no-cwd-file" })
+    ya.emit("quit", {})
   end,
 }
