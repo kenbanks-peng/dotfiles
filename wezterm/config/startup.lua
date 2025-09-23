fd2977d74ef810523d8efeda6c028d859a43ceec
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-09-22 22:21:33 -0400

    Update startup.lua

--- wezterm/config/startup.lua
+++ wezterm/config/startup.lua
@@ -1,4 +1,4 @@
 return {
-  default_prog = { 'zsh', '-l', '-i', '-c', 'f; exec zsh' },
+  -- default_prog = { 'zsh', '-l', '-i', '-c', 'f; exec zsh' },
   default_cwd = '/Users/kenbanks/Software/',
 }
