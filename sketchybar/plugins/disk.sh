de17e5306292fd8035dac5a0a5697abc509189f9
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-10-09 10:45:08 -0400

    dotfiles backup on 2025-10-09 10:45:08

--- sketchybar/plugins/disk.sh
+++ sketchybar/plugins/disk.sh
@@ -2,7 +2,10 @@
 
 source "$CONFIG_DIR/env.sh"
 
-percentage=$(df -H ~ | grep -E '^(/dev/).' | awk '{ printf ("%s\n", $5) }')
+# Sum all macOS APFS volumes (Data + System + Preboot + VM)
+used=$(df -H | awk '/disk4s[1346]/ {gsub("G","",$3); sum+=$3} END {printf "%.1f", sum}')
+total=200
+percentage=$(awk -v u="$used" -v t="$total" 'BEGIN {printf "%d%%", (u/t)*100}')
 
 sketchybar -m --set "$NAME" label="$percentage"
 
