cd027d2d7d4faf290fbbfe04433f282932e906f7
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-10-15 11:01:48 -0400

    commit

--- sketchybar/plugins/helpers/yabai.sh
+++ sketchybar/plugins/helpers/yabai.sh
@@ -45,7 +45,8 @@ yabai_is_dialog() {
   local window_info=$(yabai -m query --windows --window "$window_id" 2>/dev/null)
 
   if [[ -z "$window_info" ]]; then
-    return 1  # if we can't get window info, the app was likely just destroyed
+    # if we can't get window info, the app was likely just destroyed
+    return 1  
   fi
 
   # Extract relevant properties
