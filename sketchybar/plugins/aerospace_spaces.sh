85e8c1dfae1194fd4e8d798d1c9c6eaace9ffbf3
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-10-15 08:33:55 -0400

    commit

--- sketchybar/plugins/aerospace_spaces.sh
+++ sketchybar/plugins/aerospace_spaces.sh
@@ -3,7 +3,6 @@
 source "$CONFIG_DIR/env.sh"
 source "$PLUGIN_DIR/helpers/aerospace.sh"
 source "$PLUGIN_DIR/helpers/sketchy.sh"
-source "$PLUGIN_DIR/helpers/yabai.sh"
 
 if [ "$SENDER" = "forced" ]; then
   for space in $(aerospace_workspaces); do
@@ -12,23 +11,23 @@ if [ "$SENDER" = "forced" ]; then
   # to initialize cache of previous window_id
   aerospace_focused_window_change
 elif [ "$SENDER" = "yabai_window_focused" ]; then
-  # Skip dialogs/popups
-  if ! yabai_is_dialog "$ID"; then
+  # Get app name to check if allowed (also checks dialogs)
+  appname=$(aerospace_appname_from_window_id "$ID")
+  if allow_app "$appname" "$ID"; then
     aerospace_focused_window_change $ID
   fi
 elif [ "$SENDER" = "aerospace_workspace_change" ]; then
   aerospace_workspace_change "$FOCUSED_WORKSPACE" "$PREV_WORKSPACE"
 elif [ "$SENDER" = "yabai_window_created" ] || [ "$SENDER" = "yabai_window_deminimized" ]; then
-  # Get app name to check if allowed
+  # Get app name to check if allowed (also checks dialogs)
   appname=$(aerospace_appname_from_window_id "$ID")
-
-  # Skip dialogs/popups and excluded apps
-  if ! yabai_is_dialog "$ID" && allow_app "$appname" "$ID"; then
+  if allow_app "$appname" "$ID"; then
     aerospace_new_window_id "$ID"
   fi
 elif [ "$SENDER" = "yabai_window_destroyed" ] || [ "$SENDER" = "yabai_window_minimized" ]; then
-  # Skip dialogs/popups
-  if ! yabai_is_dialog "$ID"; then
+  # Get app name to check if allowed (also checks dialogs)
+  appname=$(aerospace_appname_from_window_id "$ID")
+  if allow_app "$appname" "$ID"; then
     aerospace_remove_window_id "$ID"
   fi
 fi
