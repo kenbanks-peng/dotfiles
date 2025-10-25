2d11802abb8f05f756f67e0d90ecdafeca6a180c
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2025-10-24 22:57:20 -0400

    commit

--- sketchybar/items/outdated.sh
+++ sketchybar/items/outdated.sh
@@ -7,20 +7,20 @@ location="right"
 sketchybar --add event update_outdated
 
 props=(
-	script="$PLUGIN_DIR/outdated.sh"
-	update_freq="$((60 * 60))" # once per hour
-	icon.drawing=off
-	icon="$ICON_LEGO"
-	label.drawing=on
-	icon.padding_left=0
+  script="$PLUGIN_DIR/outdated.sh"
+  update_freq="$((10 * 60))" # once every 10 minutes
+  icon.drawing=off
+  icon="$ICON_LEGO"
+  label.drawing=on
+  icon.padding_left=0
   icon.padding_right=0
   label.padding_left=0
   label.padding_right=2
   label.width=16
   label.align=center
-	background.padding_left=0
+  background.padding_left=0
   background.padding_right=0
 )
 sketchy_add_item outdated "$location" \
-	--set outdated "${props[@]}" \
-	--subscribe outdated update_outdated mouse.clicked
+  --set outdated "${props[@]}" \
+  --subscribe outdated update_outdated mouse.clicked
