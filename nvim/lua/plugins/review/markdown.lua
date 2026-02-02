834b2ccd88876486e94cc1d0db0e1b73b857c6bf
kenbanks-peng <kenbanks-peng.kenbanks.peng@gmail.com>
2026-02-02 09:24:33 -0500

    .

--- nvim/lua/plugins/review/markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    opts = {},
-    ft = { 'markdown', 'norg', 'rmd', 'org', 'vimwiki', 'Avante' },
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-  },
-}
