d4db168da87a6d47a709e46ea8e01c01a9e2f5e6
Ken Banks <kenbanks.peng@gmail.com>
2026-03-12 17:30:11 -0400

    commit

---
+++ sketchybar/items/separator.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Creates a pipe separator item
+# Usage: SEPARATOR_NAME=sep_1 source $ITEM_DIR/separator.sh
+
+source "$PLUGIN_DIR/helpers/sketchy.sh"
+
+location="right"
+
+props=(
+  icon="$ICON_PIPE"
+  icon.color="$OVERLAY0"
+  icon.font="$NERD:18"
+  icon.padding_left=4
+  icon.padding_right=4
+  label.drawing=off
+  background.padding_left=0
+  background.padding_right=0
+)
+
+sketchy_add_item "$SEPARATOR_NAME" "$location" \
+  --set "$SEPARATOR_NAME" "${props[@]}"
