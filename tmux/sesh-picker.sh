4f3beb4b8b85c7831bf3c038fec3aca5f9340b47
Ken Banks <kenbanks.peng@gmail.com>
2026-03-30 00:53:53 -0400

    .

--- tmux/sesh-picker.sh
+++ tmux/sesh-picker.sh
@@ -3,7 +3,7 @@
 sesh connect "$(
   sesh list --icons | fzf-tmux -p 50%,30% \
     --no-sort --ansi --border-label ' sesh ' --prompt '⚡  ' \
-    --header '                      ^x kill session   ^f find' \
+    --header '                  ^x kill session   ^f find   (see ^t and ^p)' \
     --bind 'tab:down,btab:up' \
     --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
     --bind 'ctrl-x:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)'
